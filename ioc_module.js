b3b092d77e9d3cd61c1afea61a9bb3a771cdd2b7
ElRaptorus <christian.werner@5minds.de>
2018-07-23 12:01:34 +0200

    Add ioc registrations for user task components

--- ioc_module.js
+++ ioc_module.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const ProcessModelExecutionEndpoint = require('./dist/commonjs/index').Endpoints.ProcessModelExecution;
+const UserTasksEndpoint = require('./dist/commonjs/index').Endpoints.UserTasks;
 
 const routerDiscoveryTag = require('@essential-projects/bootstrapper_contracts').routerDiscoveryTag;
 
@@ -11,6 +12,15 @@ function registerInContainer(container) {
     .tags(routerDiscoveryTag);
 
   container.register('ManagementApiProcessModelExecutionController', ProcessModelExecutionEndpoint.ProcessModelExecutionController)
+    .dependencies('ManagementApiService')
+    .singleton();
+
+  container.register('ManagementApiUserTaskRouter', UserTasksEndpoint.UserTaskRouter)
+    .dependencies('ManagementApiUserTaskController')
+    .singleton()
+    .tags(routerDiscoveryTag);
+
+  container.register('ManagementApiUserTaskController', UserTasksEndpoint.UserTaskController)
     .dependencies('ManagementApiService')
     .singleton();
 }
