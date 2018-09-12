c8eadc421e80d90d943e9307cb8dfc711890e725
ElRaptorus <christian.werner@5minds.de>
2018-09-12 09:43:31 +0200

    :sparkles: Implement getProcessModelForCorrelation endpoint

--- src/endpoints/correlations/correlation_controller.ts
+++ src/endpoints/correlations/correlation_controller.ts
@@ -1,4 +1,10 @@
-import {Correlation, IManagementApiService, ManagementContext, ManagementRequest} from '@process-engine/management_api_contracts';
+import {
+  Correlation,
+  IManagementApiService,
+  ManagementContext,
+  ManagementRequest,
+  ProcessModelExecution,
+} from '@process-engine/management_api_contracts';
 
 import {Response} from 'express';
 
@@ -21,6 +27,16 @@ export class CorrelationController {
     const context: ManagementContext = request.managementContext;
 
     const result: Array<Correlation> = await this.managementApiService.getAllActiveCorrelations(context);
+
+    response.status(this.httpCodeSuccessfulResponse).json(result);
+  }
+
+  public async getProcessModelForCorrelation(request: ManagementRequest, response: Response): Promise<void> {
+    const context: ManagementContext = request.managementContext;
+    const correlationId: string = request.params.correlation_id;
+
+    const result: ProcessModelExecution.ProcessModel =
+      await this.managementApiService.getProcessModelForCorrelation(context, correlationId);
 
     response.status(this.httpCodeSuccessfulResponse).json(result);
   }
