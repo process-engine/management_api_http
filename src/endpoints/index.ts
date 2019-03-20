d7a1db4b43f4a3cc454263fef52ab30269f12f04
Steffen <Steffen.knaup@5minds.de>
2019-03-20 11:06:17 +0100

    :sparkles: Add Events for Call Activities

--- src/endpoints/index.ts
+++ src/endpoints/index.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import * as CallActivityEndpoint from './call_activity/index';
 import * as CorrelationEndpoint from './correlations/index';
 import * as EmptyActivityEndpoint from './empty_activities/index';
 import * as EventEndpoint from './events/index';
@@ -15,4 +16,5 @@ export namespace Endpoints {
   export import ProcessModels = ProcessModelEndpoint;
   export import UserTasks = UserTaskEndpoint;
   export import ManualTasks = ManualTasksEndpoint;
+  export import CallActivity = CallActivityEndpoint;
 }
