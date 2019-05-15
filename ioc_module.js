4b8fb60a3c63396bd273b5d2227f9bb06e845406
Steffen <Steffen.knaup@5minds.de>
2019-05-15 12:12:58 +0200

    :sparkles: Add CallActivitySocket Endpoint to IocContainer

--- ioc_module.js
+++ ioc_module.js
@@ -5,6 +5,7 @@ const EmptyActivityEndpoint = require('./dist/commonjs/index').Endpoints.EmptyAc
 const EventEndpoint = require('./dist/commonjs/index').Endpoints.Event;
 const HeatmapEndpoint = require('./dist/commonjs/index').Endpoints.Heatmap;
 const ProcessModelsEndpoint = require('./dist/commonjs/index').Endpoints.ProcessModels;
+const CallActivityEndpoints = require('./dist/commonjs/index').Endpoints.CallActivity;
 const UserTasksEndpoint = require('./dist/commonjs/index').Endpoints.UserTasks;
 const ManualTasksEndpoint = require('./dist/commonjs/index').Endpoints.ManualTasks;
 
@@ -78,6 +79,11 @@ function registerInContainer(container) {
     .singleton();
 
   container.register('ManagementApiUserTaskSocketEndpoint', UserTasksEndpoint.UserTaskSocketEndpoint)
+    .dependencies('EventAggregator', 'IdentityService', 'ManagementApiService')
+    .singleton()
+    .tags(socketEndpointDiscoveryTag);
+
+  container.register('ManagementApiCallActivitySocketEndpoint', CallActivityEndpoints.CallActivitySocketEndpoint)
     .dependencies('EventAggregator', 'IdentityService', 'ManagementApiService')
     .singleton()
     .tags(socketEndpointDiscoveryTag);
