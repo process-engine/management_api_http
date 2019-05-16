952edb78002c872d8d540f00bac730495152a3b0
Alex <alexanderkasten@users.noreply.github.com>
2019-05-16 10:18:23 +0200

    :truck: Rename getTokensForFlowNodeByInstanceId to getTokensForFlowNodeByProcessInstanceId
    Co-Authored-By: Christian W. <christian.werner@5minds.de>

--- src/endpoints/heatmap/heatmap_router.ts
+++ src/endpoints/heatmap/heatmap_router.ts
@@ -46,7 +46,7 @@ export class HeatmapRouter extends BaseRouter {
     this.router.get(restSettings.paths.getProcessModelLog, wrap(controller.getProcessModelLog.bind(controller)));
     this.router.get(restSettings.paths.getProcessInstanceLog, wrap(controller.getProcessInstanceLog.bind(controller)));
     this.router.get(restSettings.paths.getTokensForFlowNode, wrap(controller.getTokensForFlowNode.bind(controller)));
-    this.router.get(restSettings.paths.getTokensForFlowNodeByInstanceId, wrap(controller.getTokensForFlowNodeByProcessInstanceId.bind(controller)));
+    this.router.get(restSettings.paths.getTokensForFlowNodeByProcessInstanceId, wrap(controller.getTokensForFlowNodeByProcessInstanceId.bind(controller)));
     this.router.get(restSettings.paths.getTokensForProcessInstance, wrap(controller.getTokensForProcessInstance.bind(controller)));
     this.router.get(restSettings.paths.getTokensForCorrelationAndProcessModel,
       wrap(controller.getTokensForCorrelationAndProcessModel.bind(controller)));
